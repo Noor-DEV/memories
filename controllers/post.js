b0524c186d05afabaeef4c2cd3e3ceed4ed13fe2
SexyMan <devnoor00@gmail.com>
2021-04-15 10:01:13 +0300

    did post get and post routes

--- controllers/post.js
+++ controllers/post.js
@@ -1,14 +1,52 @@
+const PostModel = require("../models/post");
+const base_url = "https://localhost:3001/";
 const getAllPost = (req, res, next) => {
-  res.status(200).json({ status: "Get All Posts working" });
+  PostModel.find()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "Error in retrieving all posts", error: err });
+    });
 };
 const getPost = (req, res, next) => {
   res.status(200).json({ status: "Get Post working" });
 };
 const createPost = (req, res, next) => {
+  const { path } = req.file;
+  //likes & tags are not a must......
+  const { title, name, message, file, likes, tags } = req.body;
+  const formattedTags = tags.split(",");
   console.log(req.body, "...........REQ-BODY..................");
   console.log(req.file, ".............FILE....................");
+  console.log(formattedTags, "..........formattedTags...........");
 
-  res.status(200).json({ status: "create Post working" });
+  const newPost = new PostModel({
+    title,
+    name,
+    message,
+    file,
+    likes,
+    tags: formattedTags,
+    file: `${base_url}/${path}`,
+  });
+  newPost
+    .save()
+    .then((result) => {
+      res.status(200).json({
+        message: "success in saving new post",
+        createdPost: result,
+      });
+    })
+    .catch((err) => {
+      console.log("Error occured when saving new post");
+      res.status(500).json({
+        message: "Error occured when saving new post",
+        error: err,
+      });
+    });
 };
 const updatePost = (req, res, next) => {
   res.status(200).json({ status: "update Post working" });
--- index.js
+++ index.js
@@ -24,7 +24,12 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/posts", postRoutes);
-
+app.use("/uploads", express.static("uploads"));
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Resource NOT FOUND!......",
+  });
+});
 app.listen(3001, () => {
   console.log("listening");
 });
