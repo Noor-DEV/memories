7c96a531f3264766c4f2af194ba1c20b211ffda4
SexyMan <devnoor00@gmail.com>
2021-04-15 11:28:17 +0300

    fixed some errors

--- index.js
+++ index.js
@@ -12,24 +12,51 @@ mongoose
     console.log("connected");
   })
   .catch((err) => {
-    console.log(
-      err,
-      "..................ERROR CONNECTING 2 DB..................................."
-    );
+    console.log(err);
   });
 const app = express();
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
-// app.use("/uploads", express.static("uploads"));
-app.get("/", (req, res, next) => {
-  res.status(200).redirect("/posts");
+
+//CORS..
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.status(200).json({});
+  }
 });
 
-app.use("/posts", postRoutes);
+//.......CORS........
 
+app.use("/uploads", express.static("uploads"));
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    info: "This is an API that offers posts that were posted by random users",
+    routes: "/posts, /posts/:id",
+    methods: "GET, POST, PATCH, DELETE",
+    post: {
+      _id: "",
+      title: "POST_TITLE",
+      name: "NAME_OF_THE_USER_THAT_POSTED_IT",
+      message: "INFO_ABOUT_POST",
+      file: "AN IMAGE URL",
+      tags: ["fashion", "sports", "tech"],
+    },
+    posts: ["CONTAINS_POST_OBJECTS..LIKE THE ONE ABOVE.."],
+  });
+});
+app.use("/posts", postRoutes);
 app.use((req, res, next) => {
-  res.status(404).json({
-    message: "Resource NOT FOUND!......",
+  const err = new Error("Resource NOT FOUND!......");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
   });
 });
 app.listen(3001, () => {
--- routes/post.js
+++ routes/post.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage,
   limits: {
-    fileSize: 1024 * 1024 * 20,
+    fileSize: 1024 * 1024 * 10,
   },
 });
 const router = express.Router();
